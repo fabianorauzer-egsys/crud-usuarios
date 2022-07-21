ad52b792ae6cdbb5dbb7dc35b5ec278c69a3bc68
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-21 15:04:36 -0300

    Criando Abstract Core Mail

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,5 @@
 <?php
+
 /** Global Configuration Override */
 
 return [
@@ -8,5 +9,17 @@ return [
         'database' => 'db_crud_usuario',
         'username' => 'postgres',
         'password' => 'Info@3004',
+    ],
+    'mail' => [
+        'name'              => 'smtp.mailtrap.io',
+        'host'              => 'smtp.mailtrap.io',
+        'port'              => 2525,
+        'connection_class'  => 'login',
+        'connection_config' => [
+            'from'     => 'zf3@teste.com',
+            'username' => '764551a3018d7c',
+            'password' => '3d775f8950a315',
+            'auth'    => 'CRAM-MD5',
+        ],
     ],
 ];
