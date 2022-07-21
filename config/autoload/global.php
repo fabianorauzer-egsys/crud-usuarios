fb8a0fb13dd3c13758a5dce09705e6599c56b0f3
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-21 10:17:39 -0300

    Conectando com o banco de dados

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,16 +1,12 @@
 <?php
-/**
- * Global Configuration Override
- *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
+/** Global Configuration Override */
 
 return [
-    // ...
+    'db' => [
+        'driver' => 'Pdo',
+        'host' => 'localhost',
+        'database' => 'db_crud_usuario',
+        'username' => 'postgres',
+        'password' => 'Info@3004',
+    ],
 ];
