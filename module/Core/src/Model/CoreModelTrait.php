dcddc1540a33a2c35b6e76dc21089e712ba1bcd6
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-21 16:40:15 -0300

    Criada classa abstrata para trabalhar com as tabelas da base

---
+++ module/Core/src/Model/CoreModelTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Core\Model;
+
+use Zend\Hydrator\ReflectionHydrator;
+
+trait CoreModelTrait
+{
+    public function exchangeArray(array $data)
+    {
+        (new ReflectionHydrator())->hydrate($data, $this);
+    }
+
+    public function getArrayCopy()
+    {
+        return (new ReflectionHydrator())->extract($this);
+    }
+}
